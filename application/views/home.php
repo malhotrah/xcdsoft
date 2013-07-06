0b39501724f2971a1059ecfadf4ecba8000d42c3
malhotrah <hitanshumalhotra@gmail.com>
2013-07-06 12:42:24 +0530

    first commit

---
+++ application/views/home.php
@@ -0,0 +1,387 @@
+<html>
+<head>
+
+</head>
+<body>
+
+
+<!-- BEGIN SLIDER
+================================================== -->
+<div class="slider">
+    <div class="container">
+        <div class="row">
+            <div class="span12">
+
+                <div class="slider-area">
+                    <ul class="slides">
+
+                        <!-- SLIDE #1
+                        ==================================== -->
+                        <li>
+                            <div class="row">
+                                <div class="span12 bold-slider textcenter">
+
+                                    <!-- slide #1 content -->
+                                    <div class="vcenter">
+                                        <h1 class="bold-heading">WE ARE VENTUS.</h1>
+                                        <div class="slider-buttons stripes">
+
+                                            <a class="btn btn-padded btn-prime drop-shadow button-curved curved-hz-1">SIGN UP</a>
+                                            &nbsp;
+                                            <div class="btn-group">
+                                                <a class="btn"><i class="button-flash"></i></a>
+                                                <a class="btn">TAKE A TOUR</a>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </li>
+
+                        <!-- SLIDE #2
+                        ==================================== -->
+                        <li>
+                            <div class="row">
+                                <div class="span12 textcenter">
+
+                                    <!-- slide #2 content -->
+                                    <div class="vcenter">
+                                        <img src="<?php echo base_url('');?>assets/images/ipads.png" alt=""/>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+
+                        <!-- SLIDE #3
+                        ==================================== -->
+                        <li>
+                            <div class="row">
+                                <div class="span12">
+
+                                    <!-- slide #3 content -->
+                                    <div class="slider-frame">
+                                        <div class="slider-frame-image" style="background-image: url(assets/images/laptop-top.png)">
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+
+                    </ul>
+                </div>
+                <div class="slider-space">
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- BEGIN MAIN BUBBLE
+================================================== -->
+<div class="container">
+    <div class="row">
+        <div class="span12 relative">
+
+            <div class="bubble">
+                <h4 class="left">CREATE A POWERFUL <span class="bold italic">SOLUTION</span> NOW</h4>
+                <a class="btn btn-padded right">GET STARTED</a>
+            </div>
+
+            <div class="bubble-triangle"></div>
+        </div>
+    </div>
+</div>
+
+<div class="bubble-stripes"></div>
+
+<!--
+================================================
+// BEGIN CONTENT
+================================================
+-->
+<div class="content">
+
+<!-- FEATURES
+================================================== -->
+<div class="container" style="margin-top: 42px">
+<div class="row">
+    <div class="span3">
+        <div class="features drop-shadow">
+
+            <div class="features-circle">
+                <img src="<?php echo base_url('assets/img/icon_mobile.png');?>" alt=""/>
+            </div>
+
+            <h4><strong>SUPER</strong> RESPONSIVE</h4>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Lorem ipsum dolor sit amet...</p>
+            <br>
+            <div class="features-read-more">
+                <a class="btn btn-small btn-prime">READ MORE</a>
+            </div>
+            <div class="features-triangle"></div>
+
+        </div>
+    </div>
+    <div class="span3">
+        <div class="features drop-shadow features-active">
+
+            <div class="features-circle">
+                <img src="<?php echo base_url('assets/img/icon_desktop.png');?>" alt=""/>
+            </div>
+
+            <h4><strong>SQUEEKY</strong> CLEAN</h4>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Lorem ipsum dolor sit amet...</p>
+            <br>
+            <div class="features-read-more">
+                <a class="btn btn-small btn-prime">READ MORE</a>
+            </div>
+
+            <div class="features-triangle"></div>
+        </div>
+    </div>
+    <div class="span3">
+        <div class="features drop-shadow">
+
+            <div class="features-circle">
+                <img src="<?php echo base_url('assets/img/icon_laptop.pn');?>g" alt=""/>
+            </div>
+
+            <h4><strong>MULTI</strong> PURPOSE</h4>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Lorem ipsum dolor sit amet...</p>
+            <br>
+            <div class="features-read-more">
+                <a class="btn btn-small btn-prime">READ MORE</a>
+            </div>
+
+            <div class="features-triangle"></div>
+        </div>
+    </div>
+    <div class="span3">
+        <div class="features drop-shadow">
+
+            <div class="features-circle">
+                <img src="<?php echo base_url('assets/img/icon_flexible.png');?>" alt=""/>
+            </div>
+
+            <h4><strong>HIGHLY</strong> FLEXIBLE</h4>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Lorem ipsum dolor sit amet...</p>
+            <br>
+            <div class="features-read-more">
+                <a class="btn btn-small btn-prime">READ MORE</a>
+            </div>
+
+            <div class="features-triangle"></div>
+        </div>
+    </div>
+</div>
+
+
+<!-- RECENT NEWS
+================================================== -->
+
+<div class="row">
+    <div class="span12 textcenter">
+        <span class="content-heading">RECENT NEWS</span>
+        <div class="content-heading-stripes"></div>
+    </div>
+
+    <div class="span4">
+        <div class="content-image">
+            <a href="<?php echo base_url('assets/images/image1.png');?>" class="fancybox">
+                <span class="content-image-overlay"></span>
+                <img src="<?php echo base_url('assets/images/image1.png');?>" alt="">
+            </a>
+        </div>
+
+        <div class="content-bubble drop-shadow curved">
+            <h5 class="heading">BLOG TITLE</h5>
+            <p>Lorem ipsum dolor sit amet, contur adip elit, sed do eiusmod tempor incididunt ut labore et dolore magna</p>
+        </div>
+        <div class="content-bubble-triangle"></div>
+    </div>
+
+    <div class="span4">
+        <div class="content-image">
+            <a href="<?php echo base_url('assets/images/image2.png');?>" class="fancybox">
+                <span class="content-image-overlay"></span>
+                <img src="<?php echo base_url('assets/images/image2.png');?>" alt="">
+            </a>
+        </div>
+
+        <div class="content-bubble drop-shadow curved">
+            <h5 class="heading">BLOG TITLE</h5>
+            <p>Lorem ipsum dolor sit amet, contur adip elit, sed do eiusmod tempor incididunt ut labore et dolore magna</p>
+        </div>
+        <div class="content-bubble-triangle"></div>
+    </div>
+
+    <div class="span4">
+        <div class="content-image">
+            <a href="<?php echo base_url('assets/images/image2.png');?>" class="fancybox">
+                <span class="content-image-overlay"></span>
+                <img src="<?php echo base_url('assets/images/image3.png');?>" alt="">
+            </a>
+        </div>
+
+        <div class="content-bubble drop-shadow curved">
+            <h5 class="heading">BLOG TITLE</h5>
+            <p>Lorem ipsum dolor sit amet, contur adip elit, sed do eiusmod tempor incididunt ut labore et dolore magna</p>
+        </div>
+        <div class="content-bubble-triangle"></div>
+    </div>
+</div>
+
+<!-- OUR SERVICES
+================================================== -->
+
+<div class="row">
+    <div class="span12 textcenter">
+        <span class="content-heading">OUR SERVICES</span>
+        <div class="content-heading-stripes"></div>
+    </div>
+
+    <div class="span3">
+        <ul class="nav nav-list" id="myTab">
+            <li class="first active"><a href="#aboutus">ABOUT US</a></li>
+            <li><a href="#profile">WHAT WE DO</a></li>
+            <li><a href="#messages">OUR TEAM</a></li>
+            <li><a href="#settings">PRICING</a></li>
+            <li><a href="#settings">HOW IT WORKS</a></li>
+            <li class="last"><a href="#settings">CAREERS</a></li>
+        </ul>
+    </div>
+
+    <div class="span9">
+        <div class="tab-content">
+
+            <div class="tab-pane active" id="aboutus">
+                <div class="row">
+                    <div class="span5 mobile-box">
+                        <h4><strong>The Process</strong></h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            <br><br>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et...</p>
+                        <br>
+                        <a class="btn btn-prime">GET STARTED</a>
+                    </div>
+
+                    <div class="span4">
+                        <div class="content-bubble drop-shadow curved">
+                            <h3><i>WITH GREAT DESIGN COMES GREAT <strong>RESPONSIBILITY</strong></i></h3>
+                            <p> - Dumbledoor</p>
+                        </div>
+                        <div class="content-bubble-triangle"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="tab-pane" id="profile"></div>
+            <div class="tab-pane" id="messages"></div>
+            <div class="tab-pane" id="settings"></div>
+        </div>
+    </div>
+</div>
+
+<!-- TESTIMONIALS
+================================================== -->
+<div class="row">
+
+    <!-- PAGE HEADING -->
+    <div class="span8 textcenter">
+        <span class="content-heading">TESTIMONIALS</span>
+        <div class="content-heading-stripes"></div>
+    </div>
+
+    <div class="span8">
+        <div class="row">
+
+            <!-- TESTIMONIAL #1 -->
+            <div class="span4">
+                <div class="content-bubble drop-shadow curved">
+                    <h5 class="heading">John Smith</h5>
+                    <p><i>�Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna sed do eiusmod tempor�</i></p>
+                </div>
+                <div class="content-bubble-triangle"></div>
+            </div>
+
+            <!-- TESTIMONIAL #2 -->
+            <div class="span4">
+                <div class="content-bubble drop-shadow curved">
+                    <h5 class="heading">John Smith</h5>
+                    <p><i>�Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna sed do eiusmod tempor�</i></p>
+                </div>
+                <div class="content-bubble-triangle"></div>
+            </div>
+
+
+
+            <!-- TESTIMONIAL #3 -->
+            <div class="span4">
+                <div class="content-bubble drop-shadow curved">
+                    <h5 class="heading">John Smith</h5>
+                    <p><i>�Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna sed do eiusmod tempor�</i></p>
+                </div>
+                <div class="content-bubble-triangle"></div>
+            </div>
+
+            <!-- TESTIMONIAL #4 -->
+            <div class="span4">
+                <div class="content-bubble drop-shadow curved">
+                    <h5 class="heading">John Smith</h5>
+                    <p><i>�Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna sed do eiusmod tempor�</i></p>
+                </div>
+                <div class="content-bubble-triangle"></div>
+            </div>
+
+        </div>
+    </div>
+
+    <!-- TABS
+    ================================================== -->
+
+
+    <div class="span4">
+
+        <ul class="nav nav-tabs" id="tabs">
+            <li class="active"><a href="#design">Design</a></li>
+            <li><a href="#production">Production</a></li>
+        </ul>
+
+        <div class="tab-content">
+            <div class="tab-pane active" id="design">
+                <div class="content-bubble">
+                    <h5 class="heading">Design</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna sed do eiusmod tempor ut labore et dolore magna sed
+                        <br><br>
+                        �Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna sed do eiusmod tempor�</p>
+                    <br>
+                </div>
+            </div>
+            <div class="tab-pane" id="production">
+                <div class="content-bubble">
+                    <h5><strong>Production</strong></h5>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna sed do eiusmod tempor ut labore et dolore magna sed
+                        <br><br>
+                        �Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna sed do eiusmod tempor�</p>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</div>
+</div>
+</div>
+
+</body>
+</html>
